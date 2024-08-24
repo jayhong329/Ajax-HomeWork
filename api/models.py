cb5ad99d04f1f00bb66ce7a4283c9e5edc9d9bf3
jayhong329 <smoja329@gmail.com>
2024-08-24 10:15:58 +0800

    資料庫下載新增

--- api/models.py
+++ api/models.py
@@ -28,6 +28,17 @@ class Categories(models.Model):
         db_table = 'categories'
 
 
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
 class Member(models.Model):
     user_id = models.AutoField(primary_key=True)
     user_name = models.CharField(max_length=20)
